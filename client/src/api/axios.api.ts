4b172d2ae5d2146137e586b63e4e7938c4a4e09a
Alexandr <ya.sacher@gmail.com>
2024-02-04 12:43:55 +0100

    Update axios.api.ts

--- client/src/api/axios.api.ts
+++ client/src/api/axios.api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getTokenFromLS } from "../helpers/localStorage.helper";
 
 export const instance = axios.create({
-    baseURL:'https://personal-budget-aawj.onrender.com/api',
+    baseURL:'https://personal-budget-h6at.onrender.com/api',
     headers: {
         Authorization: `Bearer ` + getTokenFromLS()
     }
